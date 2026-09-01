f739d56e129b0b4f7a7b534dfcd92f988193eb23
theluqmn <theluqmn@outlook.com>
2026-09-02 07:34:38 +0800

    added configuration file mechanic

--- README.md
+++ README.md
@@ -8,7 +8,15 @@ their own server components.
 - [ ] Monitor resource consumption
 - [ ] Manage your own backend services
 - [ ] Domain management
+- [x] Serve static files
+
+## Installation
 
 ## Technicality
+
+Mainframe can be configured via a `config.json` file, which you include when
+starting the server (eg: `mainframe config.json`). If its not specified, it defaults
+to `./config.json`. You can refer to the [json file](https://github.com/theluqmn/mainframe/blob/main/config.json)
+at the repo which includes everything you can configure.
 
 ## Notes
--- main.go
+++ main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"encoding/json"
 	"log"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	log.Println("gello world!")
+	
+	// fetch configuration
+	configPath := "./config.json"
+	if len(os.Args) > 1 {
+		configPath = os.Args[1]
+	}
+	log.Println("loading config:", configPath)
 
+	file, err := os.Open(configPath)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	var config map[string]any
+	err = json.NewDecoder(file).Decode(&config)
+	if err != nil {
+		log.Fatalf("error decoding config: %v", err)
+	}
+	log.Println("config loaded")
+	
+	// initialise echo server
 	e := echo.New()
 	e.HideBanner = true
 	e.HidePort = true
 
-	e.Static("/static", "../_static")
+	// start server
+	port := "8080"
+	if configPort, ok := config["port"]; ok {
+		port = fmt.Sprintf("%v", configPort)
+	}
 
-	e.Start(":8080")
+	log.Printf("starting at port :%s", port)
+	err = e.Start(":"+port)
+	if err != nil {
+		log.Fatalf("shut down unexpectedly: %v", err)
+	}
 }
