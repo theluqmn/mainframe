1a4da165310721fd4d640f2b0cbb19258a2d59d6
theluqmn <theluqmn@outlook.com>
2026-09-01 15:28:57 +0800

    static site serving

--- README.md
+++ README.md
@@ -12,4 +12,3 @@ their own server components.
 ## Technicality
 
 ## Notes
-
--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"log"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	log.Println("gello world!")
+
+	e := echo.New()
+	e.HideBanner = true
+	e.HidePort = true
+
+	e.Static("/static", "../_static")
+
+	e.Start(":8080")
 }
