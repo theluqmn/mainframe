85d5e692916fdccb298b4e6de52bc68023a6389d
theluqmn <theluqmn@outlook.com>
2026-09-03 18:10:11 +0800

    attempting another fix

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@ set -e
 echo ""
 echo "Mainframe Installer"
 echo ""
-echo "Welcome to a basic CLI-based somewhat interactive installer for Mainframe, a
-web OS ahh server management software."
+echo "Welcome to a basic CLI-based somewhat interactive installer for Mainframe, a web OS server management software."
 echo ""
 echo "Website: https://theluqmn.hackclub.app/mainframe"
 echo "Repository: https://github.com/theluqmn/mainframe"
@@ -20,7 +19,7 @@ if [ -z "$mainframe_folder" ]; then
     mainframe_folder="./"
 fi
 
-read -p "(2/3) Where to create a data folder? (./): " data_folder
+read -p "(2/3) Where to create a data folder? (./data): " data_folder
 if [ -z "$data_folder" ]; then
     data_folder="./data"
 fi
@@ -33,9 +32,8 @@ fi
 echo ""
 echo "Web configuration"
 echo ""
-echo "This will be required to access the web OS. The address is either
-the domain that you have pointed to the server, or the static public IPv4 address"
-read -p "Enter an address (heheheeee.com): " hostname
+echo "This will be required to access the web OS. The address is either the domain that you have pointed to the server, or the static public IPv4 address."
+read -p "Enter an address (example.com): " hostname
 if [ -z "$hostname" ]; then
     hostname="127.0.0.1"
 fi
@@ -48,7 +46,7 @@ cd "$mainframe_folder"
 curl -L -o mainframe https://github.com/theluqmn/mainframe/releases/latest/download/mainframe-linux
 chmod +x mainframe
 
-mkdir -p "../$data_folder"
+mkdir -p "$data_folder"
 echo "{\"port\": $port, \"data\": \"$data_folder\", \"hostname\": \"$hostname\"}" > config.json
 
 echo ""
